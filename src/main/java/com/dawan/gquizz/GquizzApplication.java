335226bf5a8a086675f0383a26d99fa7f04878f9
AbdelkaderBoulebene <boulebene.abdelkaderpro@gmail.com>
2024-09-09 14:21:24 +0200

    initialize project with spring

---
+++ src/main/java/com/dawan/gquizz/GquizzApplication.java
@@ -0,0 +1,13 @@
+package com.dawan.gquizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GquizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GquizzApplication.class, args);
+	}
+
+}
