ebef3b39251e9b1d88ef3a0538c5de1526d750ab
Quentin <53335284+kavneluxedy@users.noreply.github.com>
2024-09-09 16:00:01 +0200

    Add lombok & mysql driver dependencies

---
+++ src/main/java/com/dawan/gquizz/repositories/IUserRepository.java
@@ -0,0 +1,14 @@
+package com.dawan.gquizz.repositories;
+
+import com.dawan.gquizz.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, String> {
+    int getScoreByCategory(String category);
+
+    List<User> findByScoreTop100();
+}
