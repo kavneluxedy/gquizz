63e4f168ce3624ae2db161b654ad7a968ec45a85
Quentin <53335284+kavneluxedy@users.noreply.github.com>
2024-09-10 16:47:28 +0200

    Fix IUserService

--- src/main/java/com/dawan/gquizz/services/IUserService.java
+++ src/main/java/com/dawan/gquizz/services/IUserService.java
@@ -4,6 +4,6 @@ import com.dawan.gquizz.entities.User;
 
 public interface IUserService {
     void create(User user);
-    void update(User user);
+    void update(String email, User user);
     void deleteById(String email);
 }
